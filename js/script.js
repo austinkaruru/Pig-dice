14c0172e00adf686fe283fb7964a77d67152eb92
austinkaruru <karuruaustine@gmail.com>
2018-07-20 13:47:40 +0300

    updated html and js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 $(document).ready(function(event) {
     $("button#dice").click(function(event) {
         clearValues();
+        Player1.newGame();
+        Player2.newGame();
+        $("#round-score-1").empty();
+        $("#die-roll-1").empty();
+        $("#round-score-2").empty();
+        $("#die-roll-2").empty();
     })
     event.preventDefault;
-})
+});
+
+
+var Player1 = "";
+var Player2 = "";
